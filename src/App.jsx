15f6a8f879dd56e3951adcaaa4e700cb0b3ef2a2
Hm Sani <hmsadikurrahman120@gmail.com>
2023-04-02 19:22:47 +0600

    assignment 8

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Blogs from "./component/Blogs/Blogs";
+import Header from "./component/Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Blogs></Blogs>
+    </div>
+  );
+}
+
+export default App;
